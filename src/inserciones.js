669c2a83def9bd8b6a1c7f8fbaf29a2835ca1de6
donoromurilloana <anabth98@gmail.com>
2020-11-10 19:37:52 +0100

    first commit

---
+++ src/inserciones.js
@@ -0,0 +1,17 @@
+db.ciudades.insertMany([
+    {_id:1, ciudad: "Sevilla", provincia:{provincia:"Sevilla",ca:"Andalucia"},habitantes:1950000},
+    {_id:2, ciudad: "Badajoz", provincia:{provincia:"Badajoz",ca:"Extremadura"},habitantes:672493},
+    {_id:3, ciudad: "Madrid", provincia:{provincia:"Madrid",ca:"Comunidad de Madrid"},habitantes:6642000},
+    {_id:4, ciudad: "Segovia", provincia:{provincia:"Segovia",ca:"Castilla y Leon"},habitantes:154095},
+    {_id:5, ciudad: "Málaga", provincia:{provincia:"Malaga",ca:"Andalucia"},habitantes:1641000},
+
+])
+
+db.ciudades.insertMany([
+    {_id:6, ciudad: "Jerez de la Frontera", provincia:{provincia:"Cadiz",ca:"Andalucia"},habitantes:212879},
+    {_id:7, ciudad: "Burgos", provincia:{provincia:"Burgos",ca:"Castilla y Leon"},habitantes:355429},
+    {_id:8, ciudad: "Valencia", provincia:{provincia:"Valencia",ca:"Comunidad Valenciana"},habitantes:2541000},
+    {_id:9, ciudad: "Huelva", provincia:{provincia:"Huelva",ca:"Andalucia"},habitantes:524576},
+    {_id:10, ciudad: "Merida", provincia:{provincia:"Badajoz",ca:"Extremadura"},habitantes:59335},
+
+])
